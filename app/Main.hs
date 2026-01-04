e6df776907aee0669e342b55cf87992de76e030a
Brian Johnson <bjthinks@gmail.com>
2026-01-03 18:27:54 -0800

    Roll a 3d6 and get the same number always

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import Control.Monad.Random
+
+d :: Int -> Int -> Rand StdGen Int
+d n s = do
+  rolls <- sequence $ replicate s $ getRandomR (1,n)
+  return $ sum rolls
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let gen = mkStdGen 42
+  let n = evalRand (3 `d` 6) gen
+  print n
